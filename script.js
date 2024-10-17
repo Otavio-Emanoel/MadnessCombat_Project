8f087d17905ddeba08c92f907a47833a5b5a6ffa
Otavio Emanoel <otavioemanuel6b@gmail.com>
2024-10-16 21:30:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+document.getElementById("play_nexus").addEventListener("click", ()=>{
+    window.open("https://www.newgrounds.com/portal/view/592473")
+})
+
+document.getElementById("watch_serie").addEventListener("click", ()=>{
+    window.open("https://www.youtube.com/playlist?list=PL4F761764995C5F15")
+})
+
+document.getElementById("watch_story").addEventListener("click", ()=>{
+    window.open("https://www.youtube.com/watch?app=desktop&v=Nk5rPSglf_w")
+})
+
+document.getElementById("newgrounds").addEventListener("click", ()=>{
+    window.open("https://www.newgrounds.com/")
+})
+
+document.getElementById("author_serie").addEventListener("click", ()=>{
+    window.open("https://krinkels.newgrounds.com/")
+})
+
+document.getElementById("facebook").addEventListener("click", ()=>{
+    window.open("https://www.facebook.com/pages/Madness-Combat/103268429727652")
+})
+
+document.getElementById("official").addEventListener("click", ()=>{
+    window.open("https://www.krinkels.net/")
+})
+
+document.getElementById("instagram").addEventListener("click", ()=>{
+    window.open("https://www.instagram.com/krin.kels?igsh=MWRzcDJsZ3N2ZzYzbA==")
+})
+
+document.getElementById("twitter").addEventListener("click", ()=>{
+    window.open("https://x.com/MRKrinkels?t=_SZJP65_VFcuHGLCvTh9ww&s=09")
+})
+
+document.getElementById("youtube").addEventListener("click", ()=>{
+    window.open("https://www.youtube.com/channel/UC64tqaIUdbAcOajtGHnV-qw")
+})
+
+document.getElementById("").addEventListener("click", ()=>{
+    window.open("")
+})
